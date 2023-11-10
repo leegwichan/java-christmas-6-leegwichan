ce55a6abdd2b31733305980a5545f229d5048a1a
DESKTOP-E5VRH4Q\cnddk <cnddkscndgus@naver.com>
2023-11-10 10:27:48 +0900

    feat (Menu) : 각 메뉴의 가격을 알려주는 기능 구현

---
+++ src/test/java/christmas/domain/plan/MenuTest.java
@@ -0,0 +1,20 @@
+package christmas.domain.plan;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class MenuTest {
+
+    @ParameterizedTest(name = "메뉴 : {0}, 가격 : {1}")
+    @CsvSource({"MUSHROOM_SOUP, 6000", "CHRISTMAS_PASTA, 25000", "ICE_CREAM, 5000", "CHAMPAGNE, 25000"})
+    @DisplayName("메뉴에 따라 가격을 알 수 있다")
+    void getPriceTest(Menu menu, int expected) {
+
+        int actual = menu.getPrice();
+
+        assertThat(actual).isEqualTo(expected);
+    }
+}
