c7749baad9a89aeaec234fb904853f61d9130ec2
leegwichan <cnddkscndgus@naver.com>
2023-11-13 01:22:17 +0900

    feat (OutputView) : 이벤트 혜택 제목 출력 기능 구현

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -5,13 +5,19 @@ import christmas.dto.PlanResultDto;
 public class OutputView {
 
     private static final String APPLICATION_TITLE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
+    private static final String EVENT_BENEFIT_TITLE_FORMAT = "12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
+
 
     public void printApplicationTitle() {
         println(APPLICATION_TITLE);
     }
 
     public void printPlanResult(PlanResultDto planResult) {
-        throw new UnsupportedOperationException();
+        printEventBenefitTitle(planResult.date());
+    }
+
+    private void printEventBenefitTitle(int date) {
+        println(EVENT_BENEFIT_TITLE_FORMAT.formatted(date));
     }
 
     public void printExceptionMessage(IllegalArgumentException exception) {
