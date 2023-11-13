5921cd48008e90ff0ebdeada612eed73b5ea35f7
DESKTOP-E5VRH4Q\cnddk <cnddkscndgus@naver.com>
2023-11-13 10:04:33 +0900

    feat (OutputView) : 할인 전 총 주문 금액 출력 기능 구현

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -2,6 +2,7 @@ package christmas.view;
 
 import christmas.domain.plan.Menu;
 import christmas.dto.PlanResultDto;
+import java.text.DecimalFormat;
 import java.util.Map;
 
 public class OutputView {
@@ -10,8 +11,11 @@ public class OutputView {
     private static final String APPLICATION_TITLE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
     private static final String EVENT_BENEFIT_TITLE_FORMAT = "12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
     private static final String ORDER_TITLE = NEW_LINE.concat("<주문 메뉴>");
+    private static final String TOTAL_PRICE_TITLE = NEW_LINE.concat("<할인 전 총주문 금액>");
 
     private static final String MENU_FORMAT = "%s %d개";
+    private static final DecimalFormat PRICE_FORMATTER = new DecimalFormat("###,###");
+    private static final String PRICE_FORMAT = "%s원";
 
     public void printApplicationTitle() {
         println(APPLICATION_TITLE);
@@ -20,6 +24,7 @@ public class OutputView {
     public void printPlanResult(PlanResultDto planResult) {
         printEventBenefitTitle(planResult.date());
         printOrder(planResult.order());
+        printTotalPrice(planResult.totalPrice());
     }
 
     private void printEventBenefitTitle(int date) {
@@ -34,6 +39,16 @@ public class OutputView {
     private void printOrder(Menu menu, int count) {
         String menuView = MenuView.findView(menu);
         println(MENU_FORMAT.formatted(menuView, count));
+    }
+
+    private void printTotalPrice(int totalPrice) {
+        println(TOTAL_PRICE_TITLE);
+        printPrice(totalPrice);
+    }
+
+    private void printPrice(int price) {
+        String priceView = PRICE_FORMATTER.format(price);
+        println(PRICE_FORMAT.formatted(priceView));
     }
 
     public void printExceptionMessage(IllegalArgumentException exception) {
